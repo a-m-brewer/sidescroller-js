460ec2a5da0286f39c5c4b1d13b52608c33ab557
Adam Brewer <ambreweruni@gmail.com>
2017-08-02 01:02:05 +0100

    camera follows

--- js/main.js
+++ js/main.js
@@ -10,18 +10,99 @@ var GAME = function() {
 
     this.drawshape = new Draw(this.context);
     this.player = new PLAYER(this.context, BLOCK_SIZE);
+
+    this.camera_speed = {
+        x: 2,
+        y: 3
+    }
+
+    this.offset_x = 0;
+    this.offset_y = 0;
+
+    this.camera = {
+        x: 0.0,
+        y: 0.0
+    }
+
+    this.max_dist_from_cen = {
+        x: this.canvas.width / 8,
+        y: this.canvas.height / 8
+    }
+
+    this.map = {
+        width: this.canvas.width * 4,
+        height: this.canvas.height * 4
+    }
+
 }
 
 GAME.prototype.move = function(dt) {
     this.player.move(dt);
-
-    if (this.player.x <= 0 || this.player.x >= this.canvas.width - this.player.x_size) {this.player.x_vel *= -1};
-    if (this.player.y <= 0 || this.player.y >= this.canvas.height - this.player.y_size) {this.player.y_vel *= -1};
+    this.camera_follow(dt);
+    if (this.player.x <= 0 || this.player.x >= this.map.width - this.player.x_size) {this.player.x_vel *= -1};
+    if (this.player.y <= 0 || this.player.y >= this.map.height- this.player.y_size) {this.player.y_vel *= -1};
 }
 
 GAME.prototype.draw = function() {
-    this.drawshape.rectangle(0, 0, this.canvas.width, this.canvas.height, "red");
+    this.drawshape.rectangle(0,0,this.canvas.width,this.canvas.height);
+
+    this.context.save();
+    this.context.translate(-this.camera.x, -this.camera.y);
+
+    this.drawshape.rectangle(0, 0, this.map.width, this.map.height, "red");
+
     this.player.draw();
+
+    this.context.restore();
+}
+
+GAME.prototype.camera_follow = function(delta) {
+    //this.camera.x = this.player.x - this.canvas.width / 2;
+    //this.camera.y = this.player.y - this.canvas.height / 2;
+    
+    this.camera_cen = {
+        x: this.camera.x + this.canvas.width/2,
+        y: this.camera.y + this.canvas.height/2
+    }
+
+    this.player_dist_from_center = {
+        x: Math.abs(this.player.x - this.camera_cen.x),
+        y: Math.abs(this.player.y - this.camera_cen.y)
+    }
+
+    if (this.player_dist_from_center.x > this.max_dist_from_cen.x) {
+        if (this.camera_cen.x < this.player.x) {
+            this.camera.x += this.camera_speed.x * delta * 60 / 1000;
+        } else {
+            this.camera.x -= this.camera_speed.x * delta * 60 / 1000;
+        }
+    }
+
+    if (this.player_dist_from_center.y > this.max_dist_from_cen.y) {
+        if (this.camera_cen.y < this.player.y) {
+            this.camera.y += this.camera_speed.y * delta * 60 / 1000;
+        } else {
+            this.camera.y -= this.camera_speed.y * delta * 60 / 1000;
+        }
+    }
+    
+    if (this.camera.x < 0) {
+        this.camera.x = 0;
+    }
+    if (this.camera.y < 0) {
+        this.camera.y = 0;
+    }
+    this.past_viewpoint = {
+        x: this.map.width - this.canvas.width, 
+        y: this.map.height - this.canvas.height
+    }
+    if (this.camera.x > this.past_viewpoint.x) {
+        this.camera.x = this.past_viewpoint.x;
+    }
+    if (this.camera.y > this.past_viewpoint.y) {
+        this.camera.y = this.past_viewpoint.y;
+    }
+    
 }
 
 GAME.prototype.timestamp = function() {
